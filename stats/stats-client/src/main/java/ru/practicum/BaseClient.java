92ac45dbc50ab2a1ae62bf3eebc5dec0ea2a83b0
VadimDomkov <vdomkov@yandex.ru>
2024-03-10 18:48:08 +0300

    feat : stat

--- stats/stats-client/src/main/java/ru/practicum/BaseClient.java
+++ stats/stats-client/src/main/java/ru/practicum/BaseClient.java
@@ -1,13 +1,12 @@
 package ru.practicum;
 
 
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.lang.Nullable;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
 import java.util.Map;
 
 public class BaseClient {
@@ -27,7 +26,7 @@ public class BaseClient {
 
 
     private <T> ResponseEntity<Object> makeAndSendRequest(HttpMethod method, String path, @Nullable Map<String, Object> parameters, @Nullable T body) {
-        HttpEntity<T> requestEntity = new HttpEntity<>(body);
+        HttpEntity<T> requestEntity = new HttpEntity<>(body, defaultHeaders());
 
         ResponseEntity<Object> statsResponse;
         try {
@@ -41,6 +40,14 @@ public class BaseClient {
         }
 
         return prepareStatsResponse(statsResponse);
+    }
+
+    private HttpHeaders defaultHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(List.of(MediaType.APPLICATION_JSON));
+
+        return headers;
     }
 
     private static ResponseEntity<Object> prepareStatsResponse(ResponseEntity<Object> response) {
