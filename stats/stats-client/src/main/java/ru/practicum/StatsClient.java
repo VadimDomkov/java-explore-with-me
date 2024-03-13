bf177531a3b3606e9fed44f31d9a6444e852bc05
VadimDomkov <vdomkov@yandex.ru>
2024-03-13 11:56:22 +0300

    feat : stat

--- stats/stats-client/src/main/java/ru/practicum/StatsClient.java
+++ stats/stats-client/src/main/java/ru/practicum/StatsClient.java
@@ -1,45 +1,40 @@
 package ru.practicum;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.client.RestTemplateBuilder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.lang.Nullable;
-import org.springframework.web.util.DefaultUriBuilderFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
 import java.util.List;
-import java.util.Map;
 
-public class StatsClient extends BaseClient {
+@Service
+@RequiredArgsConstructor
+public class StatsClient {
+    private final WebClient webClient;
 
-    public StatsClient(@Value("${stats-server.url}") String urlServer, RestTemplateBuilder builder) {
-        super(
-                builder
-                        .uriTemplateHandler(new DefaultUriBuilderFactory(urlServer))
-                        .requestFactory(HttpComponentsClientHttpRequestFactory::new)
-                        .build()
-        );
+    public void addRequest(StatRequestDto requestDto) {
+        webClient.post()
+                .uri("/hit")
+                .bodyValue(requestDto)
+                .retrieve()
+                .bodyToMono(Object.class)
+                .block();
     }
 
-    public ResponseEntity<Object> addHit(StatRequestDto dto) {
-        return post("/hit", dto);
-    }
-
-    public ResponseEntity<Object> getStats(String start, String end, @Nullable List<String> uris, Boolean unique) {
-        Map<String, Object> params = Map.of("start", encodeTime(start),
-                "end", encodeTime(end),
-                "unique", unique);
-
-        if (uris == null) {
-            params.put("uris", uris);
-        }
-
-        return get("/stats?start={start}&end={end}&unique={unique}&uris={uris}", params);
-    }
-
-    private String encodeTime(String data) {
-        return URLEncoder.encode(data, StandardCharsets.UTF_8);
+    public ResponseEntity<List<StatResponseDto>> getStats(String start, String end, List<String> uris, Boolean unique) {
+        return webClient.get()
+                .uri(uriBuilder -> {
+                    return uriBuilder.path("/stats")
+                            .queryParam("start", start)
+                            .queryParam("end", end)
+                            .queryParam("uris", uris)
+                            .queryParam("unique", unique)
+                            .build();
+                })
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<ResponseEntity<List<StatResponseDto>>>() {
+                })
+                .block();
     }
 }
